a400fe9212e2fd250416d51ae4e0c9d452cee452
unknown <archie.burstow@outlook.com>
2018-06-03 16:50:53 +0100

    added documentation

--- src/morale/MoraleManager.java
+++ src/morale/MoraleManager.java
@@ -7,24 +7,47 @@ public class MoraleManager {
 	private ArrayList<Modifier> modifiers;
 	
 	
+	/*
+	 * creates a new empty list of modifiers
+	 */
 	public MoraleManager()
 	{
 		modifiers = new ArrayList<Modifier>();
 	}
 	
+	/*
+	 *creates a new list of modifiers using the parsed list 
+	 *@param m a list of modifiers
+	 */
 	public MoraleManager(ArrayList<Modifier> m)
 	{
 		modifiers = m;
 	}
+	
 
+	/*
+	 * adds a modifier to the list
+	 * @param m a modifier
+	 */
 	public void addModifier(Modifier m) {
 		modifiers.add(m);
 	}
 
+	/*
+	 * creates and adds a modifier to the list
+	 * @param n the name of the modifier
+	 * @param m the modifier value
+	 * @param d the description of the modifier
+	 */
 	public void addModifier(String n, double m, String d) {
 		modifiers.add(new Modifier(n, m, d));
 	}
 
+	
+	/*
+	 * remove a modifier by it's name
+	 * @param n the name of the modifier 
+	 */
 	public void removeByName(String n) {
 		for (Modifier m : modifiers) {
 			if (m.getName().equals(n)) {
--- src/units/Person.java
+++ src/units/Person.java
@@ -13,12 +13,31 @@ public class Person {
 	private Date dob;
 	public MoraleManager mm;
 
+	
+	/*
+	 * Creates a new person with name n and date of birth d
+	 * creates empty morale manager
+	 * 
+	 * @param n the name of the person
+	 * @param d the person's date of birth
+	 * @return nothing
+	 */
+	
 	public Person(String n, Date d) {
 		name = n;
 		dob = d;
 		mm = new MoraleManager();
 	}
 
+	/*
+	 * Creates a new person with name n and date of birth d
+	 * creates empty morale manager
+	 * uses parsed string to create the date object
+	 * 
+	 * @param n the name of the person
+	 * @param d the person's date of birth
+	 * @return nothing
+	 */
 	public Person(String n, String d) {
 		name = n;
 		try {
@@ -29,13 +48,21 @@ public class Person {
 		mm = new MoraleManager();
 	}
 	
+	/*
+	 * Creates a new person with name n and date of birth d
+	 *uses parsed list of modifiers to create morale manager
+	 * 
+	 * @param n the name of the person
+	 * @param d the person's date of birth
+	 * @param m a list of modifiers
+	 * @return nothing
+	 */
 	public Person(String n, Date d, ArrayList<Modifier> m)
 	{
 		name = n;
 		dob = d;
 		mm = new MoraleManager(m);
 	}
-
 	public String getName() {
 		return name;
 	}
